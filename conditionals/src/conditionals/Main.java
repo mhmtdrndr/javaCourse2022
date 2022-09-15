3981e01bc60e113b89da401b9ffd01efe84dcdc0
Mehmet Derindere <83140766+mhmtdrndr@users.noreply.github.com>
2022-09-15 14:04:42 +0300

    Add files via upload

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int sayi = 150;
+		
+		if (sayi > 100) {
+			System.out.println("Sayı 100'den büyüktür.");
+		} else if (sayi < 100) {
+			System.out.println("Sayı 100'den küçüktür.");
+		} else {
+			System.out.println("Sayı 100'e eşittir.");
+		}
+
+	}
+
+}
