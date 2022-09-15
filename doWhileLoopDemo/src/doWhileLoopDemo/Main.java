a6216dd6f73c9c1322cb3c5868704b032a45e3db
Mehmet Derindere <83140766+mhmtdrndr@users.noreply.github.com>
2022-09-15 15:11:07 +0300

    Add files via upload

---
+++ doWhileLoopDemo/src/doWhileLoopDemo/Main.java
@@ -0,0 +1,18 @@
+package doWhileLoopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int i = 10;
+		
+		do {
+			System.out.println(i);
+			i--;
+		} while(i >= 0);
+		
+		System.out.println("Do-While döngüsü bitti");
+
+	}
+
+}
