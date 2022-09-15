e3849e11a28e5a77547a8c9a6bf29ded92cb1399
Mehmet Derindere <83140766+mhmtdrndr@users.noreply.github.com>
2022-09-15 14:57:47 +0300

    Add files via upload

---
+++ whileLoopDemo/src/whileLoopDemo/Main.java
@@ -0,0 +1,18 @@
+package whileLoopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		while(i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		System.out.println("döngü bitti");
+
+	}
+
+}
