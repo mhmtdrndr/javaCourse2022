0553d8032ac4bab54c45c54f32075417b269c576
Mehmet Derindere <83140766+mhmtdrndr@users.noreply.github.com>
2022-09-15 15:32:14 +0300

    Add files via upload

---
+++ arraysDemo/src/arraysDemo/Main.java
@@ -0,0 +1,25 @@
+package arraysDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String kategoriler[] = {
+				"Elektronik",
+				"Moda",
+				"Ev, Yaşam, Kırtasiye, Ofis",
+				"Oto, Bahçe, Yapı Market",
+				"Anne, Bebek, Oyuncak",
+				"Spor, Outdoor",
+				"Kozmetik, Kişisel Bakım",
+				"Süpermarket, Pet Shop",
+				"Kitap, Müzik, Film, Hobi"
+		};
+
+		for (int i = 0; i < kategoriler.length; i++) {
+			System.out.println(kategoriler[i]);
+		}
+		
+	}
+
+}
